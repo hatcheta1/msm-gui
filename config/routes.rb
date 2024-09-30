7c051ba3afeed9a79474b20818555ddf916c1c77
Ashanti Hatchett <ahatchett02@gmail.com>
2024-09-30 00:52:48 +0000

    added an edit form for movies

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  post("/insert_director_record", { :controller => directors, :action => "create" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/insert_movie_record", { :controller => "movies", :action => "create" })
   get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
+  post("/modify_movie_record/:path_id", { :controller => "movies", :action => "update" })
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
